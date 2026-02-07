e1173d60da6ada4e6379df1902eb5d6f2ab43529
daramonyyem <yem.daramony@gmail.com>
2026-02-07 19:55:15 +0700

    update message

--- src/index.js
+++ src/index.js
@@ -14,5 +14,5 @@ app.use("/api", paymentRoute);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`🚀 Server running on port ${PORT}`);
+  console.log(`🚀 Server running testing on port ${PORT}`);
 });
